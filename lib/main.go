12f89dc186e2893df3ce6c51463f93992ccace34
10sr <8slashes+git@gmail.com>
2018-02-13 13:50:19 +0900

    Add more command examples

--- lib/main.go
+++ lib/main.go
@@ -14,4 +14,20 @@ func Main(num *int){
 		log.Fatal(err)
 	}
 	fmt.Printf("lib.Main: %s\n", strings.TrimSpace(string(out)));
+
+	cmd_getroot := exec.Command("git", "rev-parse", "--show-toplevel")
+	out_getroot, err_getroot := cmd_getroot.Output()
+	if err_getroot != nil {
+		log.Fatal(err_getroot)
+	}
+	fmt.Printf("lib.Main: %s\n", strings.TrimSpace(string(out_getroot)))
+	git_toplevel := strings.TrimSpace(string(out_getroot))
+
+	cmd_tgt := exec.Command("pwd")
+	cmd_tgt.Dir = git_toplevel
+	out_tgt, err_tgt := cmd_tgt.Output()
+	if err_tgt != nil {
+		log.Fatal(err_tgt)
+	}
+	fmt.Printf("lib.Main: %s\n", strings.TrimSpace(string(out_tgt)))
 }
